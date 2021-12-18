18c6388fe4a720a63659391e67050a94a1c2b75e
Muhammad Irtiza <m7.irtiza@gmail.com>
2021-12-18 15:58:01 +0500

    Added facts for immediate family members.

---
+++ Prolog Programs/21L-7232-facts.pl
@@ -0,0 +1,10 @@
+% Version: SWI-Prolog 8.4.1
+% Name: Muhammad Irtaza
+% Enrollment Number: 21L - 7232
+name(muhammad_irtaza).
+enrollment(21L-7232).
+father(irtaza,muslim).
+mother(irtaza,raheela).
+brother(irtaza,ahmed).
+brother(irtaza,ali).
+brother(irtaza,hassaan).
