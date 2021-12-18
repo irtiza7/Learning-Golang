bd361c757896f1061c3717f6b27f430457858aca
Muhammad Irtiza <m7.irtiza@gmail.com>
2021-12-18 17:18:11 +0500

    Added some more facts.

--- Prolog Programs/21L-7232-facts.pl
+++ Prolog Programs/21L-7232-facts.pl
@@ -1,10 +1,34 @@
 % Version: SWI-Prolog 8.4.1
 % Name: Muhammad Irtaza
-% Enrollment Number: 21L - 7232
-name(muhammad_irtaza).
-enrollment(21L-7232).
+% Roll Number: 21L-7232
+
+male(irtaza).
+male(muslim).
+male(ali).
+male(ahmed).
+male(hassaan).
+female(raheela).
+
 father(irtaza,muslim).
 mother(irtaza,raheela).
 brother(irtaza,ahmed).
 brother(irtaza,ali).
 brother(irtaza,hassaan).
+
+parent(irtaza,muslim).
+parent(ali,muslim).
+parent(ahmed,muslim).
+parent(hassaan,muslim).
+parent(irtaza,raheela).
+parent(ali,raheela).
+parent(ahmed,raheela).
+parent(hassaan,raheela).
+
+
+/*
+sibling(Person,Sib):-
+    Brother = brother(Person,Brother),
+    Sister = sister(Person,Sister),
+    Sib = Brother,
+    Sib = Sister.
+*/
