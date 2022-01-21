7dc529380e822d39873035326c578d40956e6e04
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-01-21 19:50:05 +0500

    Scheme practice 2.

--- Scheme Programs/main.scm
+++
@@ -1,5 +0,0 @@
-#lang scheme
-(define a 10)
-(define b 20)
-(define add (lambda (x y) (+ x y)))
-(add a b)
---
+++ Scheme Programs/practice.scm
@@ -0,0 +1,7 @@
+#lang scheme
+
+
+;SUM OF ELEMENTS OF LIST
+;(define marks (list 1 2 3 4))
+;(define sum (lambda (lst) (if (null? lst) 0 (+ (car lst) (sum (cdr lst))))))
+;(sum marks)
